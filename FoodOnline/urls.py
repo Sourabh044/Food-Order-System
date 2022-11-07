c39e1b350c44cec755a2c83842b007f723d2f8e2
“Sourabh044” <srb.sourabh.044@gmail.com>
2022-11-07 11:16:42 +0530

    Food Crud

--- FoodOnline/urls.py
+++ FoodOnline/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView, name="Home"),
-    path("accounts/", include("accounts.urls")),
+    path("", include("accounts.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
